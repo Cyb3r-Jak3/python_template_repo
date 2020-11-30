799e28ab1e779eb0736013ccdc5efc305493e6f5
Cyb3r Jak3 <jake@jwhite.network>
2020-11-29 19:24:37 -0800

    Dropped 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -42,8 +42,6 @@ setup(
             "Natural Language :: English",
             "Programming Language :: Python :: 3 :: Only",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.4",
-            "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
